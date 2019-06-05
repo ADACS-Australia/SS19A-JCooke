b62325ca54863c2042ab1e07ce0b26dbdf692f07
Shibli Saleheen <ssaleheen@swin.edu.au>
2019-06-06 02:22:52 +1000

    pagination and sorting initial

--- dwfsearch/utility/api.py
+++ dwfsearch/utility/api.py
@@ -7,6 +7,60 @@ import json
 def search(query_parts, search_columns):
     search_results = [
         {
+            'id': '1',
+            'name': 'name 1',
+            'ra': '01:01:01',
+            'dec': '1',
+        },
+        {
+            'id': '2',
+            'name': 'name 2',
+            'ra': '02:02:02',
+            'dec': '2',
+        },
+        {
+            'id': '3',
+            'name': 'name 3',
+            'ra': '03:03:03',
+            'dec': '3',
+        },
+        {
+            'id': '4',
+            'name': 'name 4',
+            'ra': '04:04:04',
+            'dec': '4',
+        },
+        {
+            'id': '5',
+            'name': 'name 5',
+            'ra': '05:05:05',
+            'dec': '5',
+        },
+        {
+            'id': '6',
+            'name': 'name 6',
+            'ra': '06:06:06',
+            'dec': '6',
+        },
+        {
+            'id': '7',
+            'name': 'name 7',
+            'ra': '07:07:07',
+            'dec': '7',
+        },
+        {
+            'id': '8',
+            'name': 'name 8',
+            'ra': '08:08:08',
+            'dec': '8',
+        },
+        {
+            'id': '9',
+            'name': 'name 9',
+            'ra': '09:09:09',
+            'dec': '9',
+        },
+        {
             'id': '10',
             'name': 'name 10',
             'ra': '10:10:10',
@@ -17,6 +71,12 @@ def search(query_parts, search_columns):
             'name': 'name 11',
             'ra': '11:11:11',
             'dec': '11',
+        },
+        {
+            'id': '12',
+            'name': 'name 12',
+            'ra': '12:12:12',
+            'dec': '12',
         }
     ]
 
--- dwfsearch/views/search_result.py
+++ dwfsearch/views/search_result.py
@@ -8,6 +8,8 @@ import codecs
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from dwfcommon.utility.paginator import Paginator
+
 from ..utility.api import search
 from ..utility.utils import (
     get_search_columns,
@@ -18,6 +20,21 @@ from ..utility.utils import (
 
 @login_required
 def cone_search_result(request):
+
+    sort = request.GET.get('sort', None)
+    page = request.GET.get('page', None)
+
+    if sort:
+        # update sort
+        pass
+
+    if page:
+        # update page
+        pass
+
+    if sort or page:
+        return redirect('cone_search_result')
+
     try:
         query_parts = pickle.loads(codecs.decode(request.session['query_parts'].encode(), "base64"))
     except KeyError:
@@ -29,11 +46,19 @@ def cone_search_result(request):
     search_columns = get_search_columns(request.user)
     display_headers = search_display_headers(search_columns)
     search_results, total = search(query_parts, search_columns)
+
+    paginator = Paginator(
+        start_index=1,
+        total=total,
+        per_page=2,
+    )
+
     return render(
         request,
         "dwfsearch/cone_search_results.html",
         {
             'search_results': process_search_results(search_results, search_columns),
             'display_headers': display_headers,
+            'paginator': paginator,
         },
     )
