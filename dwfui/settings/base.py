f2181eb8bd1e3dec11d453d3210c09d3241decfd
Shibli Saleheen <ssaleheen@swin.edu.au>
2019-06-07 00:51:23 +1000

    added job controller

--- dwfui/settings/base.py
+++ dwfui/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_hpc_job_controller',
     'dwfjob',
     'dwfsearch',
     'dwfcommon',
